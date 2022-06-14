2a6ee3cf2cf1367e6c3a533166a0ec807ed08dc1
HemantTrellance <hkhinchi.trellance@gmail.com>
2022-06-14 14:42:41 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import React, { Component } from 'react';
 import Icon from '@mdi/react';
-import { mdiAccount } from '@mdi/js';
+import {
+  mdiAccountFilter,
+  mdiAlarmBell,
+  mdiAirballoon,
+  mdiAccessPointNetwork,
+} from '@mdi/js';
+
 import './style.css';
 export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
+      <Icon path={mdiAccountFilter} title="User Profile" size={1} color="red" />
+      &nbsp;
       <Icon
-        path={mdiAccount}
+        path={mdiAccessPointNetwork}
         title="User Profile"
         size={1}
-        horizontal
-        vertical
-        rotate={90}
-        color="red"
-        spin
+        color="black"
       />
+      <Icon path={mdiAlarmBell} title="User Profile" size={1} color="red" />
+      &nbsp;
+      <Icon path={mdiAirballoon} title="User Profile" size={1} color="black" />
     </div>
   );
 }
